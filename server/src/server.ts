5bc545fbc0dd71f9038360c26124d1fb88d057c3
AidanBoissonneault <58098123+AidanBoissonneault@users.noreply.github.com>
2026-03-17 18:01:33 -0400

    Established MySQL connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,33 @@
 import 'dotenv/config'
+import mysql from 'mysql2'
 import app from "./app.js"
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+// extract .env data
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, PORT } = process.env
+
+// validate .env data
+if (!DB_HOST || !DB_USER || !DB_PASSWORD || !DB_NAME) {
+  throw new Error('Missing required database environment variables')
+}
+
+// connect to MySQL
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+})
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`Error connecting to MySQL: ${err.stack}`);
+    return;
+  }
+
+  console.log("connected to MySQL database")
+});
+
+// listening port
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
